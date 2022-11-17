57982597e1ce1e897bd74bcd786fa8138f5dc4e8
Jan Ferreira <janderson_ferreira@outlook.com>
2022-11-17 20:39:49 -0300

    Primeira classe Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		Position pos = new Position(3, 5);
 		
-		System.out.print("f");
+		System.out.println(pos);
 
 	}
 
